fd8678995cb8749dde1ca28ee3d0cfac74f032d6
Jeferson Pimentel <jerfinho.2013@hotmail.com>
2022-02-06 20:54:11 -0300

    pequenas alterações

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Router, Route } from "react-router";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
+      <Route>path="/" componet ={App}</Route>
+    </Router>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
